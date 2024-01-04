1f1a4fab132be589b0da91582d07c551a18cdf5b
Rafael Adelino Candido <112955398+CandidoRafael@users.noreply.github.com>
2024-01-04 08:22:00 -0300

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <p className='bg-slate-700 font-bold text-blue-400'>Olá</p>
+    </div>
+  )
+}
+
+export default Dashboard
